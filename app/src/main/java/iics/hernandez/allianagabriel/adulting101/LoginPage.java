1dd75acd1f4e3030bc84a2920c38be5e91579670
flavian <2015086506@ust-ics.mygbiz.com>
2018-11-10 16:34:40 +0800

    Initial commit

---
+++ app/src/main/java/iics/hernandez/allianagabriel/adulting101/LoginPage.java
@@ -0,0 +1,21 @@
+package iics.hernandez.allianagabriel.adulting101;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+    public void sign(View v) {
+       startActivity(new Intent(LoginPage.this, SignupPage.class));
+    }
+
+
+}
